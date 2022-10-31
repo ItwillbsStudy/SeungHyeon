ae329a6032c1087d068ad6f9764a89a50155e4d3
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-31 19:08:16 +0900

    Update 피자 나눠 먹기 （1）.java

--- Week_2/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ Week_2/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = n % 7 == 0 ? n / 7 : n / 7 + 1;
+        int answer = n % 7 == 0 ? n / 7 : n / 7 + 1; // 인원수를 7로 나누었을 때 남은 인원이 있으면 +1
         return answer;
     }
 }
