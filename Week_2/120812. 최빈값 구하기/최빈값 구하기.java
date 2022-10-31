c73ff0bc8c55a7cec117e744b8a2d808078717a2
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-31 15:44:20 +0900

    Update 최빈값 구하기.java

--- Week_2/120812. 최빈값 구하기/최빈값 구하기.java
+++ Week_2/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,25 +1,25 @@
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        int[] cnt = new int[1001];
-        int[] dup = new int[1001];
+        int[] cnt = new int[1001]; // 원소의 개수를 카운트할 배열
+        int[] dup = new int[1001]; // 원소의 개수의 중복을 체크 할 배열
         
-        for(int x : array){
+        for(int x : array){ // 원소를 인덱스로 사용해 그 원소의 값을 증가
             cnt[x]++;
         }
         
         int max = 0;
         int index = 0;
         
-        for(int i = 0; i < cnt.length; i++){
-            if(max <= cnt[i]){
-                max = cnt[i];
-                dup[max]++;                
-                index = i;
+        for(int i = 0; i < cnt.length; i++){ // cnt 배열에서 최대값 구하기, i는 array배열의 원소값, cnt는 그 원소의 개수
+            if(max <= cnt[i]){ // cnt[i]가 max보다 크거나 작을 때
+                max = cnt[i]; // max에 cnt[i]값을 넣어줌
+                dup[max]++; // dup배열에 원소의 개수를 카운트 한 값을 증가                
+                index = i; // 인덱스에 현재 i넣어줌
             }
         }
-        if(dup[max] > 1) return -1;
+        if(dup[max] > 1) return -1; // dup[max]가 두개 이상 나왔으면 -1
         
-        return index;
+        return index; // 아니면 이거 리턴
     }
 }
