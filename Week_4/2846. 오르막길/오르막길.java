2c13391b5d6e9661088cacff0215d0718705d1ce
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-11-14 12:41:27 +0900

    Update 오르막길.java

--- Week_4/2846. 오르막길/오르막길.java
+++ Week_4/2846. 오르막길/오르막길.java
@@ -8,13 +8,13 @@ public class Main {
       int n = sc.nextInt();
       
       int before = sc.nextInt();
-      list.add(0);
+      list.add(0); // 첫 오르막 크기는 0
       
       for(int i = 1; i < n; i++){
           int current = sc.nextInt();
           
-          if(current > before) list.add(list.get(i - 1) + current - before);
-          else list.add(0);
+          if(current > before) list.add(list.get(i - 1) + current - before); // current > before
+          else list.add(0); // current <= before
           
           before = current;
       }
@@ -22,3 +22,13 @@ public class Main {
       System.out.print(Collections.max(list));
     }
 }
+
+// current > before 일 때 current - before로 오르막 크기를 list에 add
+// list에 들어있는 전의 오르막 크기도 더해줘야 함
+// 더해주지않으면 current와 before의 차이 값만 나옴
+
+// 그 나머지 경우인 같거나 작을 때 0을 add해주어 오르막 크기 초기화
+
+// before에 current를 넣고 다음 진행
+
+// list의 max 출력
